1b6c0a84dc96fab2f79c5abcaeee2dbf67fa5c36
SyDan1day <dannot4life@gmail.com>
2026-07-19 07:52:00 +0800

    07 done

--- main.go
+++ main.go
@@ -7,15 +7,10 @@ import (
 func main() {
 	var n int
 	fmt.Scan(&n)
-	// Print FizzBuzz / Fizz / Buzz / n.
-	switch {
-	case n%5 == 0 && n%3 == 0:
-		fmt.Println("FizzBuzz")
-	case n%5 == 0 && n%3 != 0:
-		fmt.Println("Buzz")
-	case n%5 != 0 && n%3 == 0:
-		fmt.Println("Fizz")
-	default:
-		fmt.Println(n)
+	// Loop and sum, then print.
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
 	}
+	fmt.Println(sum)
 }
