b6af563dc96ab3062d46c2598238ff675a04b527
SyDan1day <dannot4life@gmail.com>
2026-07-19 21:22:21 +0800

    13 done

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Point struct {
-	X, Y int
-}
-
-func (p *Point) distance(q *Point) int {
-	return (p.X-q.X)*(p.X-q.X) + (p.Y-q.Y)*(p.Y-q.Y)
+func increment(n *int) {
+	// dereference and increment
+	*n++
 }
 
 func main() {
-	var x1, y1, x2, y2 int
-	fmt.Scan(&x1)
-	fmt.Scan(&y1)
-	fmt.Scan(&x2)
-	fmt.Scan(&y2)
-	// Build two Points and print squared distance.
-	p1 := Point{x1, y1}
-	p2 := Point{x2, y2}
-	fmt.Println(p1.distance(&p2))
+	var x int
+	fmt.Scan(&x)
+	increment(&x)
+	fmt.Println(x)
 }
