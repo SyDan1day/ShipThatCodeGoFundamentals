de68aa996142c5195b6489fca805096c1cbf18e9
SyDan1day <dannot4life@gmail.com>
2026-07-19 09:00:31 +0800

    09 done

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
-
-func square(n int) int {
-	// Return n * n.
-	return n * n
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	fmt.Println(square(n))
+	r := bufio.NewReader(os.Stdin)
+	line, _ := r.ReadString('\n')
+	line = strings.TrimRight(line, "\r\n")
+	// Try to parse and print the result.
+	result, err := strconv.Atoi(line)
+	if err != nil {
+		fmt.Println("bad")
+		return
+	}
+	fmt.Printf("ok %d\n", result)
 }
